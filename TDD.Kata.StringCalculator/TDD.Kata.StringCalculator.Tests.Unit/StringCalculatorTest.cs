fd9ded2672f651f131f15722e74cf84bcac9c776
Matias Alberto Barriento <matias.barriento@neoris.cxnetworks.net>
2017-08-30 10:27:37 -0300

    Add method with test

---
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator.Tests.Unit/StringCalculatorTest.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+
+namespace TDD.Kata.StringCalculator.Tests.Unit
+{
+    public class StringCalculatorTest
+    {
+        private StringCalculator Target;
+
+        public StringCalculatorTest()
+        {
+            this.Target = new StringCalculator();
+        }
+
+        [Fact]
+        public void TestEmptyString()
+        {
+            //Arrange
+            //Your set up!.
+            string numbers = string.Empty;
+
+            //Act
+            //Your principal action to test.
+            int result = this.Target.add(numbers);
+
+            //Assert
+            //Your expected value.
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void TestOneNumberString()
+        {
+            //Arrange
+            //Your set up!.
+            string numbers = "1";
+
+            //Act
+            //Your principal action to test.
+            int result = this.Target.add(numbers);
+
+            //Assert
+            //Your expected value.
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void TestTwoNumbersString()
+        {
+            //Arrange
+            //Your set up!.
+            string numbers = "1,2";
+
+            //Act
+            //Your principal action to test.
+            int result = this.Target.add(numbers);
+
+            //Assert
+            //Your expected value.
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator/StringCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Kata.StringCalculator
+{
+    public class StringCalculator
+    {
+        public int add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            int sum = 0;
+            foreach (string number in numbers.Split(',')) {
+
+                sum += Convert.ToInt32(number);
+            }
+
+
+            return sum;
+        }
+    }
+}
